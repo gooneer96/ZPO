266de34cc7c76e293749a4b5482b9f6cf382cc9d
Tomek <tomekowczarski96@gmail.com>
2020-03-22 22:32:15 +0100

    javadoc

--- lab02/zad03/src/com/company/DrawTool.java
+++ lab02/zad03/src/com/company/DrawTool.java
@@ -1,14 +1,29 @@
 package com.company;
 
 
-
+/**
+ * Class <code>DrawTool</code>> enable us to use object of this class to draw a ruler.
+ */
 public class DrawTool {
 
+    /**
+     *
+     *
+     * @param tickLength value telling us how much "--" to draw
+     * @param lengthMax  is max of nesting
+     */
     public static void drawOneTick (int tickLength,int lengthMax)
     {
         drawOneTick(tickLength,-1,lengthMax);
     }
 
+    /**
+     *
+     *
+     * @param tickLength value telling us how much "--" to draw
+     * @param tickLabel  value of current drawn label
+     * @param lengthMax  is max of nesting
+     */
     public static void drawOneTick(int tickLength, int tickLabel, int lengthMax) {
         for(int i =lengthMax;i>=tickLength;i--)
             System.out.print(" ");
@@ -17,6 +32,13 @@ public class DrawTool {
         if (tickLabel >= 0) System.out.print(" " + tickLabel);
         System.out.print("\n");
     }
+
+    /**
+     *
+     *
+     * @param tickLength value telling us how much "--" to draw
+     * @param lengthMax  is max of nesting, value needed in <code>drawOneTick</code> to fill blank spaces from left in each row in output
+     */
     public static void drawTicks(int tickLength, int lengthMax) {
         if (tickLength > 0) {
             drawTicks(tickLength- 1,lengthMax);
@@ -26,6 +48,13 @@ public class DrawTool {
             drawTicks(tickLength- 1,lengthMax);
         }
     }
+
+    /**
+     * 
+     *
+     * @param nLabels     value represent max value on ruler, from 0 to nLabels
+     * @param majorLength is max of nesting, value needed in <code>drawOneTick</code> to fill blank spaces from left in each rowin output
+     */
     public static void drawRuler(int nLabels, int majorLength) {
         drawOneTick(majorLength, 0,majorLength); // tick 0
         for (int i = 1; i <= nLabels; i++) {
